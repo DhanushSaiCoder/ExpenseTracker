551c1c9bb1ce64c75e277b877b941b0fd1cb38a0
Dhanush Sai Nayak <dhanushsai1467@gmail.com>
2024-11-27 21:37:52 +0530

    post to signupUsers

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const router = express.Router()
+const User = require('../models/User')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path')
 
-router.post('/',(req,res) => {
-    
+router.post('/',async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+       const isUser = await User.findOne({email})
+
+       if(isUser){
+        res.status(400).send('User already exists')
+        return;
+       }
+
+       const newUser = new User({username,email,password})
+       await newUser.save()
+
+       res.status(201).status('User created successfully')
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+
 })
-  
-module.exports= router
+
+module.exports = router
