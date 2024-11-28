d53faa4d3d24cd51e3a8a3e8e280b885b6748719
Dhanush Sai Nayak <dhanushsai1467@gmail.com>
2024-11-28 22:01:24 +0530

    +patch +delete expense

--- routes/expenses.js
+++ routes/expenses.js
@@ -3,6 +3,16 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const { Expense, validateExpense } = require("../models/Expense");
 
+router.get('/', async (req, res) => {
+    try {
+        const expenses = await Expense.find()
+        res.send(expenses)
+    } catch (error) {
+        console.error("Error getting expense:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+})
+
 router.post("/", async (req, res) => {
     const { error } = validateExpense(req.body);
     if (error) return res.status(400).send({ message: error.details[0].message });
@@ -27,4 +37,34 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.patch('/:id', async (req, res) => {
+    try {
+        const result = await Expense.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        if (!result) {
+            return res.status(404).send({ message: "Expense not found" });
+        }
+
+        res.send(result);
+    } catch (error) {
+        console.error("Error patching expense:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const result = await Expense.findByIdAndDelete(req.params.id, req.body)
+
+
+    if (!result) {
+        return res.status(404).send({ message: "Expense not found" });
+    }
+
+        res.send(result);
+    } catch (error) {
+        console.error("Error patching expense:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+})
 module.exports = router;
